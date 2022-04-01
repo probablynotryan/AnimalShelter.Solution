3fb93be31b4dfa532b3047d6ee82fde5e3331c4e
Ryan Bass <theryanbass@gmail.com>
2022-04-01 11:59:05 -0700

    added put/getid method responses to controller

--- AnimalShelter.Api/Controllers/AnimalsController.cs
+++ AnimalShelter.Api/Controllers/AnimalsController.cs
@@ -7,8 +7,8 @@ using System.Linq;
 
 namespace AnimalShelterApi.Controllers
 {
-[Route("api/[controller]")]
-    [ApiController]
+  [Route("api/[controller]")]
+  [ApiController]
     public class AnimalsController : ControllerBase
     {
       private AnimalShelterApiContext _db;
@@ -44,6 +44,48 @@ namespace AnimalShelterApi.Controllers
         }
 
             return await query.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Animal>> GetAnimal(int id)
+        {
+          var animal = await _db.Animals.FindAsync(id);
+
+          if (animal == null)
+          {
+            return NotFound();
+          }
+
+          return animal;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Animal animal)
+        {
+          if (id != animal.AnimalId)
+          {
+            return BadRequest();
+          }
+
+          _db.Entry(animal).State = EntityState.Modified;
+
+          try
+          {
+            await _db.SaveChangesAsync();
+          }
+          catch (DbUpdateConcurrencyException)
+          {
+            if (!AnimalExists(id))
+            {
+              return NotFound();
+            }
+            else
+            {
+              throw;
+            }
+          }
+
+          return NoContent();
         }
     }
 }
