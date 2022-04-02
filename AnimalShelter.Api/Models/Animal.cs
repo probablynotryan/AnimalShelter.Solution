e48189d803afff278037bf8b71785890551cd686
Ryan Bass <theryanbass@gmail.com>
2022-04-02 15:47:48 -0700

    add gender / required to animal model

--- AnimalShelter.Api/Models/Animal.cs
+++ AnimalShelter.Api/Models/Animal.cs
@@ -5,10 +5,16 @@ namespace AnimalShelterApi.Models
   public class Animal
   {
     public int AnimalId {get; set;}
+    [Required]
     public string Name {get; set;}
+    [Required]
     public int Age {get; set;}
+    [Required]
     public string Species {get; set;}
+    [Required]
     public string Description {get; set;}
+    [Required]
+    public string Gender {get; set;}
 
   }
 }
